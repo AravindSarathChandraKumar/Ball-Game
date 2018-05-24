519dc231393c8eddc83553c37e6cdeb4d26bd51b
AravindSarathChandraKumar <aravinds95@gmail.com>
2018-05-24 11:26:03 +0530

    added  player paddle  and its functions

--- main.js
+++ main.js
@@ -6,7 +6,23 @@ var ballSpeedX = 5;
 var ballSpeedY = 4;
 
 var paddle1Y = 250;
+var paddle2Y = 250;
 const PADDLE_HEIGHT = 100;
+const PADDLE_THICKNESS = 10;
+
+function calculateMousePos(evt){
+    
+    var rect = canvas.getBoundingClientRect();
+    var root = document.documentElement;
+    var mouseX = evt.clientX - rect.left - root.scrollLeft;
+    var mouseY = evt.clientY - rect.top - root.scrollTop;
+    return{
+      x:mouseX,
+      y:mouseY    
+    };
+}
+
+
  window.onload = function(){
      
      canvas = document.getElementById("game");
@@ -16,16 +32,44 @@ const PADDLE_HEIGHT = 100;
          draw();
          move();
      },1000/framesPerSecond);
+     
+     canvas.addEventListener('mousemove',function(evt){
+         
+         var mousePos = calculateMousePos(evt);
+             paddle1Y = mousePos.y-(PADDLE_HEIGHT/2);
+         
+         
+     });
  } 
+ 
+ function ballReset(){
+
+     ballSpeedX = -ballSpeedX;     
+     ballX = canvas.width/2;
+     ballY = canvas.height/2;
+ }
  
  function move(){
      
      ballX=ballX + ballSpeedX;
      if(ballX < 0){
-         ballSpeedX = -ballSpeedX;
+         
+         if(ballY > paddle1Y && ballY < paddle1Y + PADDLE_HEIGHT){
+             
+             ballSpeedX = -ballSpeedX;
+         }else{
+        
+        ballReset();
+     }
      }
      if(ballX > canvas.width){
-         ballSpeedX = -ballSpeedX;
+         if(ballY > paddle2Y && ballY < paddle2Y + PADDLE_HEIGHT){
+             
+             ballSpeedX = -ballSpeedX;
+         }else{
+        
+          ballReset();
+     }
      }
      ballY=ballY + ballSpeedY;
      if(ballY < 0){
@@ -39,8 +83,13 @@ const PADDLE_HEIGHT = 100;
 function draw(){
     // this line blank out the scree
     drawRect(0,0,canvas.width,canvas.height,'black');
-    //this will be paddle 
-    drawRect(0,210,10,100,'white');
+    //this will be player paddle 
+    drawRect(0,paddle1Y,PADDLE_THICKNESS,PADDLE_HEIGHT,'white');
+    
+    //this will be compputer paddle 
+    drawRect(canvas.width-PADDLE_THICKNESS,paddle2Y,PADDLE_THICKNESS,PADDLE_HEIGHT,'white');
+    
+    
     //this will be  ball
     drawBall(ballX,ballY,10,'white');
 }
